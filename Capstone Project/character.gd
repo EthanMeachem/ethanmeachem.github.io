53c18af3be5a95c76633416c48df0fb0e178c332
EthanMeachem <ethanmeachem@gmail.com>
2025-05-21 12:17:13 -0600

    imported terrain and then fixed issues with movement

--- Capstone Project/character.gd
+++ Capstone Project/character.gd
@@ -1,4 +1,4 @@
-extends RigidBody3D
+extends CharacterBody3D
 
 var mouse_sensitivity := 0.004
 var twist_input := 0.0
@@ -6,6 +6,8 @@ var pitch_input := 0.0
 
 @onready var twist_pivot := $TwistPivot
 @onready var pitch_pivot := $TwistPivot/PitchPivot
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,7 +19,9 @@ func _process(delta):
 	input.x = Input.get_axis("MoveLeft","MoveRight")
 	input.z = Input.get_axis("MoveForward","MoveBack")
 	
-	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
+	velocity = ((twist_pivot.basis * input).normalized() * 600.0 * delta)
+	apply_floor_snap()
+	move_and_slide()
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
