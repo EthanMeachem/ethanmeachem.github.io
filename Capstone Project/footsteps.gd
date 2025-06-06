8f5f69a313dc99cf010b27b887a1ae36a8465f81
EthanMeachem <ethanmeachem@gmail.com>
2025-06-06 12:16:47 -0600

    started game over scene

--- Capstone Project/footsteps.gd
+++ Capstone Project/footsteps.gd
@@ -1,7 +1,10 @@
 extends Area3D
+var token = 0
 
 func _on_body_entered(body):
-	if body == get_node("/root/" + get_tree().current_scene.name + "/Character"):
+	if body == get_node("/root/" + get_tree().current_scene.name + "/Character") and token <= 0:
 		print("Footsteps played")
 		$Foosteps.play()
 		monitoring = false
+		token+=1
+		
--- Capstone Project/jumpscare.gd
+++ Capstone Project/jumpscare.gd
@@ -1,12 +1,14 @@
 extends Area3D
+var token = 0
 
 var animationPlayer
 func _ready():
 	animationPlayer = get_node("/root/" + get_tree().current_scene.name + "/HouseInterior/Jumpscares/AnimationPlayer")
 
 func _on_body_entered(body):
-	if body == get_node("/root/" + get_tree().current_scene.name + "/Character"):
+	if body == get_node("/root/" + get_tree().current_scene.name + "/Character") and token <= 0:
 		print("Jumpscare played")
 		$ScareSound.play()
 		animationPlayer.play("JumpScare")
+		token+=1
 		
